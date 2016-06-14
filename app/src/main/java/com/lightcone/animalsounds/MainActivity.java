e88ba532441a25a10f0564d00e067f42099dcbfc
guidrymwg <guidry@utk.edu>
2016-06-13 20:04:15 -0400

    minor change

--- app/src/main/java/com/lightcone/animalsounds/MainActivity.java
+++ app/src/main/java/com/lightcone/animalsounds/MainActivity.java
@@ -64,7 +64,7 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
     @Override
     public void onPause() {
         super.onPause();
-        // Release the MediaPlayer if going into background
+        // Release the MediaPlayer if going into background if it isn't null
         if(mp != null) mp.release();
     }
 }
